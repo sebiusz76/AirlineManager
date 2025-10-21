55afddc99e89dc4c7067b308a2c00c8dbecd282c
Sebastian Stachurski <sstachurski@gmail.com>
2025-10-21 23:42:28 +0200

    Add project files.

---
+++ AirlineManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AirlineManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
