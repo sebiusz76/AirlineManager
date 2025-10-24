a3a03f31a6fe41a08347b6d4fef3f6140bd942fd
Sebastian Stachurski <sstachurski@gmail.com>
2025-10-24 21:00:41 +0200

    Password reset in the admin panel

---
+++ AirlineManager.Models/ViewModel/AdminSetPasswordViewModel.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AirlineManager.Models.ViewModel
+{
+    public class AdminSetPasswordViewModel
+    {
+        public string Id { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
