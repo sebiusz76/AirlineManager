8fecef3b9254e953d19df040ad0d49ba2b74fee1
Sebastian Stachurski <sstachurski@gmail.com>
2025-10-26 22:33:35 +0100

    SuperAdmin has the option to disable 2FA for a given user.

--- AirlineManager.Models/ViewModel/AdminUserViewModel.cs
+++ AirlineManager.Models/ViewModel/AdminUserViewModel.cs
@@ -6,9 +6,12 @@ namespace AirlineManager.Models.ViewModel
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
         // Single role (highest in hierarchy) assigned to the user
         public string Role { get; set; }
+
         public bool IsLockedOut { get; set; }
         public DateTimeOffset? LockoutEnd { get; set; }
+        public bool TwoFactorEnabled { get; set; }
     }
 }
